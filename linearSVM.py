b6dc26c3e1921c892e802c88d5f63cbffe09bdb5
Noah Snelson <noah.snelson@protonmail.com>
2018-04-06 02:07:15 -0800

    Added analytical gradient to SVM, with proper hyperparam tuning accuracy of ~36% achieved

--- linearSVM.py
+++ linearSVM.py
@@ -1,64 +1,48 @@
 import numpy as np
 from loadCIFAR import loadCIFAR
-
+import matplotlib.pyplot as plt
 class linearSVM():
 
 	# class is responsible for storing training data set & labels
 	# and all hyperparamaters
-	def __init__(self, X, Y, W, d=1.0):
+	def __init__(self, X, Y, W, reg=5e4, learning_rate=1e-8):
 		self.Xtr = X
 		self.Ytr = Y
 		self.W = W
-		self.delta = d
-		self.gradients = np.zeros(self.W.shape)
+		self.reg = reg
+		self.learning_rate = learning_rate
 
 	# uses gradient descent to optimize loss function with regards to weight
 	# X is training images, Y is training labels
-	def train(self, X, Y):
-		step_size = 1 ** -6 
-		print('Training...\n')
-		for i in range(X.shape[0]):
-			weight_gradients = self.numerical_gradient(X[i], Y[i])
-			self.W += -weight_gradients * step_size
-			print(self.W)
+	def train(self, X, Y, batchSize):
+		randLoc = np.random.choice(X.shape[0], batchSize, replace=True)
+		batchData = X[randLoc]
+		batchLabels = Y[randLoc]
+		loss, weight_gradients = self.gradient(batchData, batchLabels)
+		self.W += weight_gradients * -self.learning_rate
+		print('Loss: {}\n'.format(loss))
+		return loss
 
 	# approximates gradient of loss function w/ respect to weights
 	# using numerical method f'(x) = ( f(x+h) - f(x) ) / h
 	def numerical_gradient(self, X , Y):
 		fx = self.loss_i(X, Y)
-		h = .00001
-		self.gradients = np.zeros(self.W.shape)
-		for j in range(self.gradients.shape[1]):
-			for i in range(self.gradients.shape[0]):
+		h = 1e-5
+		gradients = np.zeros(self.W.shape)
+		for j in range(gradients.shape[1]):
+			for i in range(gradients.shape[0]):
 				oldW = self.W[i][j]
 				self.W[i][j] = oldW + h
 				fxh = self.loss_i(X, Y)
-				self.gradients[i][j] = (fxh - fx) / h
+				gradients[i][j] = (fxh - fx) / h
 				self.W[i][j] = oldW
-		return self.gradients
-			
-	# finds gradient of loss function w/ respect to weights using derivative
-	# of loss function found w/ calculus
-	def gradient(self, X, Y):
-		for i in range(self.gradients.shape[0]):
-			if i == Y:
-				sumOutOfMargin = 0
-				for j in range(self.gradients.shape[0]):
-					if(self.loss_i(X,Y) > 0):
-						sumOutOfMargin += 1
-				self.gradients[i] = sumOutOfMargin*-X
-			else:
-				if(self.loss_i(X,Y) > 0):
-					self.gradients[i] = X
-				else:
-					self.gradients[i] = 0
-		return self.gradients
+		return gradients
 
 	# returns SVM loss of X training image and Y training label
 	def loss_i(self, X, Y):
-		scores = self.W.dot(X)
+		scores = X.dot(self.W)
 		trueScore = scores[Y]
-		loss = np.maximum(0, scores - trueScore + self.delta)
+		loss = np.maximum(0, scores - trueScore + 1)
 		loss[Y] = 0
 		return np.sum(loss) 
 
@@ -66,16 +50,29 @@ class linearSVM():
 	def regularize(self):
 		return np.sum(self.W * self.W)
 
-	# returns loss over entire data set
-	def lossTotal(self, X, Y):
-		sumLoss = 0
-		for i in range(X.shape[0]):
-			sumLoss += self.loss_i(X[i],Y[i])
-		return sumLoss/X.shape[0] + self.regularize()			
+	# returns loss over entire data set and gradient, X is all training images and Y is 
+	# training labels
+	def gradient(self, X, Y):
+		scores = X.dot(self.W) 
+		trueScores = scores[np.arange(scores.shape[0]), Y]
+		margins = np.maximum(0, scores - np.matrix(trueScores).T + 1)
+		margins[np.arange(X.shape[0]), Y] = 0
+		loss = np.mean(np.sum(margins, axis=1))
+		loss += .5 * self.reg * np.sum(self.W * self.W)
+		
+		binary = margins
+		binary[margins > 0] = 1
+		row_sum = np.sum(margins, axis=1)
+		binary[np.arange(X.shape[0]), Y] = -row_sum.T
+		dW = np.dot(X.T, binary)
+		dW /= X.shape[0]
+		dW += self.reg * self.W
+
+		return loss, dW
 
 	# returns scores of classes in X image
 	def score(self, X):
-		scores = self.W.dot(X)
+		scores = X.dot(self.W)
 		return scores 
 	
 	# returns highest score of classes in X image, aka predicted class
@@ -85,29 +82,60 @@ class linearSVM():
 
 def main():
 	trainingData, trainingLabels, testData, testLabels = loadCIFAR()
-	weights = np.random.rand(10, 3072) * .001
+	validationData, validationLabels = trainingData[30000:], trainingLabels[30000:]
+	trainingData, trainingLabels = trainingData[:30000], trainingLabels[:30000]
+	weights = np.random.rand(3072, 10) * .00001
 	clf = linearSVM(trainingData, trainingLabels, weights)
-	test_set = testData[:100]
-	trainingData = trainingData[:512]
+	batchSize = 512
 	untrainedCorrect = 0
 	trainedCorrect = 0
-	trainingEpochs = 10
+	trainingEpochs = 5000
+	trainingLosses = np.zeros(trainingEpochs)
+	learningRates = [1e1, 1e-1, 1e-3, 1e-5, 1e-7, 1e-10]
+	regs = [1e5, 1e3, 1e1, 1e-1, 1e-3, 1e-5, 1e-7, 1e-10]
+	validationAccs = np.zeros((len(learningRates),len(regs)))
 
+	untrainedLoss, untrainedGradient = clf.gradient(trainingData, trainingLabels)
 	for i in range(trainingData.shape[0]):
 		prediction = clf.evaluate(trainingData[i])
 		if( prediction == trainingLabels[i]):
 			untrainedCorrect += 1
-	
-	for i in range(trainingEpochs):
-		clf.train(trainingData, trainingLabels)
 
-	for i in range(test_set.shape[0]):
-		prediction = clf.evaluate(test_set[i])
+	for i in range(trainingEpochs):
+		print('Training epoch {}:'.format(i))
+		trainingLosses[i] = clf.train(trainingData, trainingLabels, batchSize)
+	'''
+	for i in range(len(learningRates)):
+		for j in range(len(regs)):
+			validationCorrect = 0
+			clf.reg = regs[j]
+			clf.learning_rate = learningRates[i]
+			clf.train(validationData, validationLabels, validationData.shape[0])
+			print("Testing learning rate {}, reg {}".format(learningRates[i],regs[j]))
+			for h in range(validationData.shape[0]):
+				prediction = clf.evaluate(validationData[h])
+				if prediction == validationLabels[h]:
+					validationCorrect += 1
+			accuracy = (validationCorrect / validationData.shape[0]) * 100
+			validationAccs[i][j] = accuracy
+			print("Accuracy: {}".format(accuracy))
+
+	print(validationAccs)
+	'''
+	trainedLoss, trainedGradient = clf.gradient(trainingData, trainingLabels)
+	for i in range(testData.shape[0]):
+		prediction = clf.evaluate(testData[i])
 		if( prediction == testLabels[i]):
 			trainedCorrect += 1
 	
+	
 	print('\nAccuracy untrained: {}%'.format(untrainedCorrect/trainingData.shape[0] * 100))
-	print('Accuracy trained: {}%\n'.format(trainedCorrect/test_set.shape[0] * 100))
+	print('Untrained loss: {}'.format(untrainedLoss))
+	print('Accuracy trained: {}%'.format(trainedCorrect/testData.shape[0] * 100))
+	print('Trained loss: {}\n'.format(trainedLoss))
+
+	plt.plot(np.arange(trainingEpochs), trainingLosses)
+	plt.show()
 
 if (__name__ == '__main__'):
 	main()
